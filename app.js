77f81d8700707ce151a656bf9b4834340ddee361
Jonas Høiby <hotmanmax@hotmail.com>
2017-01-23 12:52:23 +0100

    moved more routes into own js files text files for more sqLite SQL rename index.js to app.js

--- app.js
+++ app.js
@@ -34,50 +34,12 @@ endpoints.forEach((e) => {
 
 [
   'default',
-  'featrelated'
+  'featrelated',
+  'characterclass',
+  'spell'
 ].map((controllerName) => {
   controller = require('./routes/' + controllerName);
   controller.setup(app, db);
-});
-
-
-/* CHARACTERCLASS ENDPOINT */
-console.log("Registering endpoint: /characterclass");
-app.get('/characterclass', (req, res) => {
-	
-	var sqlParams = sqlHelper.getSqlParams(req);
-
-	var result = [];
-	
-	db.serialize(() => {
-		db.each(sqlHelper.addSqlParam(`SELECT dnd_characterclassvariant.id AS guid, * FROM dnd_characterclassvariant 
-				 			 LEFT OUTER JOIN dnd_characterclass ON dnd_characterclassvariant.character_class_id = dnd_characterclass.id 
-							 LEFT OUTER JOIN dnd_rulebook ON dnd_characterclassvariant.rulebook_id = dnd_rulebook.id`, sqlParams), function(err, row) {
-			result.push(row);
-		}, () => {
-			res.json(result);
-		});
-	});
-});
-
-/* SPELL ENDPOINT */
-console.log("Registering endpoint: /spell");
-app.get('/spell', (req, res) => {
-	
-	var sqlParams = sqlHelper.getSqlParams(req);
-
-	var result = [];
-	
-	db.serialize(() => {
-		db.each(sqlHelper.addSqlParam(`SELECT dnd_spell.id AS guid, dnd_spell.rulebook_id AS rulebook, dnd_spell.school_id AS school, dnd_spell.sub_school_id AS subschool, * FROM dnd_spell
-							 LEFT OUTER JOIN dnd_rulebook ON dnd_spell.rulebook_id = dnd_rulebook.id
-							 LEFT OUTER JOIN dnd_spellschool ON dnd_spell.school_id = dnd_spellschool.id
-							 LEFT OUTER JOIN dnd_spellsubschool ON dnd_spell.sub_school_id = dnd_spellsubschool.id`, sqlParams), function(err, row) {
-			result.push(row);
-		}, () => {
-			res.json(result);
-		});
-	});
 });
 
 app.listen(3000);
---
+++ routes/characterclass.js
@@ -0,0 +1,24 @@
+var sqlHelper = require('../helpers/sql');
+
+module.exports = {
+    setup: (app, db) => {
+        /* CHARACTERCLASS ENDPOINT */
+        console.log("Registering endpoint: /characterclass");
+        app.get('/characterclass', (req, res) => {
+
+            var sqlParams = sqlHelper.getSqlParams(req);
+
+            var result = [];
+
+            db.serialize(() => {
+                db.each(sqlHelper.addSqlParam(`SELECT dnd_characterclassvariant.id AS guid, * FROM dnd_characterclassvariant 
+                                     LEFT OUTER JOIN dnd_characterclass ON dnd_characterclassvariant.character_class_id = dnd_characterclass.id 
+                                     LEFT OUTER JOIN dnd_rulebook ON dnd_characterclassvariant.rulebook_id = dnd_rulebook.id`, sqlParams), function(err, row) {
+                    result.push(row);
+                }, () => {
+                    res.json(result);
+                });
+            });
+        });
+    }
+}
